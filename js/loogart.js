fcd9d0d857e30f4e0f61d0c666499da8bcbb087f
Chris Soueidan <chris@loogart.com>
2017-01-05 12:36:48 -0500

    created loog a day page, added lazy loading for images, updated about link in portfolio content section

--- js/loogart.js
+++ js/loogart.js
@@ -1,11 +1,14 @@
-        $(window).scroll(function () {
-            if ($(document).scrollTop() > 50) {
-                $('nav').addClass('shrink');
-            } else {
-                $('nav').removeClass('shrink');
-            }
-        });
-
-$('.project-1-starbucks').parallaxScroll({
-  friction: 0.4
+//navigation animation       
+$(window).scroll(function () {
+    if ($(document).scrollTop() > 50) {
+        $('nav').addClass('shrink');
+    } else {
+        $('nav').removeClass('shrink');
+    }
+});
+//lazy load
+$(function () {
+    $("img.img-responsive").lazyload({
+        effect: "fadeIn"
+    });
 });
