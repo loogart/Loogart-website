521fac7b6e08138a920ca94a945800fac1cdba65
Chris Soueidan <chris.soueidan@gmail.com>
2017-01-06 18:32:05 -0500

    fixed (finally) the code for the contact form via php.

--- php/contactform.php
+++ php/contactform.php
@@ -2,84 +2,26 @@
 
 // basic settings section
 $sendto ='chris@loogart.com';
-$subject = 'Loogart website message';
-$iserrormessage = 'Error! check out following problems:';
-$thanks = "Thanks! We'll contact you soon.";
-
-$emptyname =  'Please enter your name.';
-$emptyemail = 'Invalid e-mail address.';
-$emptymessage = 'Please enter your message.';
-
-$alertname =  'Invalid name format. Please do not use special characters in your name.';
-$alertemail = 'Invalid e-mail format, proper format is: yourname@domain.com';
-$alertmessage = "Please do not use special characters in your message. Standard url's should work fine.";
-
-
-$alert = '';
-$iserror = 0;
+$subject = 'Loogart.com message';
 
 // cleaning the post variables
 function clean_var($variable) {$variable = strip_tags(stripslashes(trim(rtrim($variable))));return $variable;}
 
-// validation of filled form
-if ( empty($_REQUEST['contact-name']) || $_REQUEST['contact-name'] == "") {
-	$iserror = 1;
-	$alert .= "<li><p>" . $emptyname . "</p></li>";
-} elseif ( preg_match( "/[][{}()*+?.\\^$|]/i", $_REQUEST['contact-name'] ) ) {
-	$iserror = 1;
-	$alert .= "<li><p>" . $alertname . "</p></li>";
-}
-
-
-if ( empty($_REQUEST['contact-email']) || $_REQUEST['contact-email'] == "Enter your e-mail address") {
-	$iserror = 1;
-	$alert .= "<li><p>" . $emptyemail . "</p></li>";
-} elseif ( !preg_match("/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,3})$/i", $_REQUEST['contact-email']) ) {
-	$iserror = 1;
-	$alert .= "<li><p>" . $alertemail . "</p></li>";
-}
-
-if ( empty($_REQUEST['contact-message']) || $_REQUEST['contact-message'] == "Your message goes here...") {
-	$iserror = 1;
-	$alert .= "<li><p>" . $emptymessage . "</p></li>";
-} elseif ( preg_match( "", $_REQUEST['contact-message'] ) ) {
-	$iserror = 1;
-	$alert .= "<li><p>" . $alertmessage . "</p></li>";
-}
-
-// if there was error, print alert message
-/*if ( $iserror==1 ) {
-
-echo "<script>
-		$(\"#message\").addClass(\"warning\").stop().slideDown(\"normal\").fadeIn(\"normal\").delay(3000).slideUp(\"normal\");
-	 </script>";
-echo "<div class=\"alert alert-block alert-danger\">";
-echo "<div class=\"alert_title\"><p><strong>" . $iserrormessage . "</strong></p></div>";
-echo "<ul class=\"unordered\">";
-echo $alert;
-echo "</ul>";
-echo "</div>";
-    
-header("Location: http://v2.loogart.com/contact.html#errorPopup");
-
-
-} else {*/
-// if everything went fine, send e-mail
+// all is good, send e-mail
 $plsubject = "=?utf-8?B?".base64_encode($subject)."?=";
-$msg = "Name: " . clean_var($_REQUEST['contact-name']) . "\n";
-$msg .= "E-mail: " . clean_var($_REQUEST['contact-email']) . "\n";
+$msg = "Name: " . clean_var($_REQUEST['first_name']) . "\n";
+$msg .= "E-mail: " . clean_var($_REQUEST['email']) . "\n";
+$msg .= "Message: \n\n" . clean_var($_REQUEST['comment']);
 
-
-$msg .= "Message: \n\n" . clean_var($_REQUEST['contact-message']);
+// email format header
 $header = "Content-type: text/plain; charset=utf-8\r\n";
-$header .= 'From:'. clean_var($_REQUEST['contact-email']);
+$header .= 'From:'. clean_var($_REQUEST['email']);
 
-
+// email content
 mail($sendto, $plsubject, $msg, $header);
 
+// following email send, bring user back to this page
 header("Location: http://v2.loogart.com#thankyou");
 
-
 die();
-//}
 ?>
