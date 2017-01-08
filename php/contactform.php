6d5f6965ce84b98e0b3347859fde4f1d70ecae9f
Chris Soueidan <chris.soueidan@gmail.com>
2017-01-08 09:08:22 -0500

    Cleaned up php code

--- php/contactform.php
+++ php/contactform.php
@@ -5,24 +5,26 @@ $sendto ='chris@loogart.com';
 $subject = 'Loogart.com message';
 
 // cleaning the post variables
-function clean_var($variable) {$variable = strip_tags(stripslashes(trim(rtrim($variable))));return $variable;}
+//function clean_var($variable) {$variable = strip_tags(stripslashes(trim(rtrim($variable))));return $variable;}
 
 // all is good, send e-mail
 $plsubject = "=?utf-8?B?".base64_encode($subject)."?=";
-$msg = "Name: " . clean_var($_REQUEST['first_name']) . "\n";
-$msg .= "Subject: " . clean_var($_REQUEST['subject']) . "\n";
-$msg .= "E-mail: " . clean_var($_REQUEST['email']) . "\n";
-$msg .= "Message: \n\n" . clean_var($_REQUEST['comment']);
+$msg = "Name: " . $_REQUEST['first_name'] . "\n";
+$msg .= "Subject: " . $_REQUEST['subject'] . "\n";
+$msg .= "E-mail: " . $_REQUEST['email'] . "\n";
+$msg .= "Message: \n\n" . $_REQUEST['comment'];
 
 // email format header
-$header = "Content-type: text/plain; charset=utf-8\r\n";
-$header .= 'From:'. clean_var($_REQUEST['email']);
+$header = "MIME-Version: 1.0" . "\r\n";
+$header .= "Content-type: text/plain; charset=utf-8\r\n";
+$header .= 'From:'. $_REQUEST['email'];
+
 
 // email content
 mail($sendto, $plsubject, $msg, $header);
 
 // following email send, bring user back to this page
-header("Location: http://v2.loogart.com#thankyou");
-
+//header("Location: http://v2.loogart.com#thankyou");
+header('Location: /index.html#thankyou');  
 die();
 ?>
