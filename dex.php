346019ebfd81f540d978e30ee33277169f8ae7d5
Chris Soueidan <chris.soueidan@gmail.com>
2017-01-24 23:02:17 -0500

    created php include scripts on test page dex.html and html templates as a test for nabber and footer

---
+++ dex.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="Loogart is a visual design studio that specializes in illustration, brand identity, small-scale animation and web design. Our work is featured in Montreal, Toronto, Ottawa and Gatineau.">
+    <meta name="author" content="Chris Soueidan of Loogart">
+    <meta property="og:title" content="Loogart">
+    <meta property="og:description" content="Montreal-based Visual Design Studio">
+    <meta property="og:url" content="http://loogart.com/">
+    <meta property="og:image" content="http://loogart.com/version2/img/loogart-screenshot.jpg">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Illustration, brand identity and web design | Loogart</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/font-awesome.css" rel="stylesheet">
+    <link href="css/zoom.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+    <!-- Fixed navbar -->
+    <?php include 'php/navbar.php';?>
+    
+    <!-- content  of page -->
+    <div class="container">
+
+        <!-- Jumbotron section -->
+
+        <div class="row">
+            <div class="container">
+                <h1 class="hidden">Loogart: Montreal visual design studio for illustration, brand identity and web design</h1>
+                <h2>Loogart is a Montreal-based visual design studio that specializes in illustration, brand identity, small-scale animation and web design.</h2>
+                <p class="cta-jumbotron">
+                    <a class="btn btn-lg btn-pink" href="contact.html" role="button">email us</a>
+                </p>
+            </div>
+        </div>
+
+        <!-- LoogADay section -->
+
+        <section class="portfolio">
+            <div class="row">
+                <div class="container">
+                    <h3>#LoogADay project</h3>
+                    <h4>A collection of pop-culture and iconic people created by Loogart on an almost daily basis.</h4>
+                    <div class="scroll">
+                        <a href="portraits-loog-a-day.html"><img class="carousel-loogs" src="img/loogs/loog-elvis-presley.gif" alt="Animated portrait of Elvis Presley by Loogart" title="Elvis presley"></a>
+                        <a href="portraits-loog-a-day.html"><img class="carousel-loogs" src="img/loogs/loog-ronald-mcdonald.gif" alt="Animated portrait of Ronald McDonalds by Loogart" title="Ronald McDonald"></a>
+                        <a href="portraits-loog-a-day.html"><img class="carousel-loogs" src="img/loogs/loog-colonel-sanders-kfc.gif" alt="Animated portrait of KFC's Colonel Sanders by Loogart" title="Colonel Sanders"></a>
+                        <a href="portraits-loog-a-day.html"><img class="carousel-loogs" src="img/loogs/loog-mr-bean.gif" alt="Animated portrait of comedian Mr. Bean by Loogart" title="Mr. Bean"></a>
+                        <a href="portraits-loog-a-day.html"><img class="carousel-loogs" src="img/loogs/loog-ice-cube-nwa.gif" alt="Animated portrait of Hip Hop Rap artist Ice Cube by Loogart" title="Ice Cube"></a>
+                        <a href="portraits-loog-a-day.html"><img class="carousel-loogs" src="img/loogs/loog-ironman.gif" alt="Animated portrait of Ironman by Loogart" title="Ironman"></a>
+                        <a href="portraits-loog-a-day.html"><img class="carousel-loogs" src="img/loogs/loog-pug.jpg" alt="Portrait of a cute pug by Loogart" title="Cute pug"></a>
+                        <a href="portraits-loog-a-day.html"><img class="carousel-loogs" src="img/loogs/loog-pk-subban.gif" alt="Animated portrait of NHL's PK Subban by Loogart" title="PK Subban"></a>
+                        <a href="portraits-loog-a-day.html"><img class="carousel-loogs" src="img/loogs/loog-bruno-mars.gif" alt="Animated portrait of pop singer Bruno Mars by Loogart" title="Bruno Mars"></a>
+                        <a href="portraits-loog-a-day.html"><img class="carousel-loogs" src="img/loogs/loog-snoop-dogg.jpg" alt="Portrait of the Godfather of Rap Snoop Dogg by Loogart" title="Snoop Dogg"></a>
+                        <a href="portraits-loog-a-day.html"><img class="carousel-loogs" src="img/loogs/loog-joker.jpg" alt="Portrait of Heath Ledger's Joker in Batman by Loogart" title="The Joker"></a>
+                        <a href="portraits-loog-a-day.html"><img class="carousel-loogs" src="img/loogs/loog-harry-potter.jpg" alt="Portrait of Harry Potter by Loogart" title="Harry Potter"></a>
+                        <a href="portraits-loog-a-day.html"><img class="carousel-loogs" src="img/loogs/loog-whoopi.jpg" alt="portrait actress Whoopi Goldberg by Loogart" title="Whoopi Goldberg"></a>
+                        <a href="portraits-loog-a-day.html"><img class="carousel-loogs" src="img/loogs/loog-spiderman.gif" alt="Animated portrait of Spiderman by Loogart" title="Spiderman"></a>
+                    </div>
+                    <h5><a href="portraits-loog-a-day.html">View all Loogs</a></h5>
+                </div>
+            </div>
+        </section>
+
+        <!-- Portfolio section -->
+
+        <section class="portfolio">
+
+            <div class="row">
+                <div class="container" id="montreal">
+                    <h3 id="selectedProjects">Selected projects</h3>
+                    <h4>A selection of awesome design projects for different clients. These projects include illustration, animation, app visual design, packaging, web graphics and print.</h4>
+                </div>
+                <div class="col-sm-6">
+                    <a href="projects/montreal-cityline-illustration.html">
+                        <span class="outer">
+                        <div class="inner portfolio-montreal"></div>
+                        <div class="portfolio-text bottom no-gradient text-dark">
+                            <h5>Montreal CityLine</h5>
+                            <p>Illustration created originally for my best friend’s living room. He of course has it hanging in his living room.</p>
+                        </div>
+                    </span>
+                    </a>
+                </div>
+                <div class="col-sm-6" id="starbucks">
+                    <a href="projects/starbucks-cityscape-illustration.html">
+                        <div class="outer">
+                            <div class="inner portfolio-starbucks"></div>
+                            <div class="portfolio-text top no-gradient text-dark">
+                                <div class="logo pull-left starbucks-logo"></div>
+                                <div class="logo-text pull-right">
+                                    <h5 class="text-green-starbucks">Starbucks</h5>
+                                    <p>Illustration created originally for my best friend’s living room. He of course has it hanging in his living room.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6" id="ets">
+                    <a href="projects/ets-cityline-illustration.html">
+                        <div class="outer">
+                            <div class="inner portfolio-ets"></div>
+                            <div class="portfolio-text bottom no-gradient text-dark">
+                                <div class="logo pull-left ets-logo"></div>
+                                <div class="logo-text pull-right">
+                                    <h5 class="text-red-ets">ÉTS</h5>
+                                    <p>Illustration designed for the Ecole de Technologie Superieure in Montreal.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-sm-6" id="car2go">
+                    <a href="projects/car2go-cityline-illustration.html">
+                        <div class="outer">
+                            <div class="inner portfolio-car2go"></div>
+                            <div class="portfolio-text top no-gradient text-light">
+                                <div class="logo-text pull-left">
+                                    <h5>car2go</h5>
+                                    <p>Illustration created for car2go Montreal produced by Station 16.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6" id="swiffer">
+                    <a href="projects/swiffer-marketing-graphics.html">
+                        <div class="outer">
+                            <div class="inner portfolio-swiffer"></div>
+                            <div class="portfolio-text bottom text-light">
+                                <div class="logo pull-left swiffer-logo"></div>
+                                <div class="logo-text pull-right">
+                                    <h5>Swiffer</h5>
+                                    <p>Montreal Moving Day social media creative.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-sm-6" id="upa">
+                    <a href="projects/upa-cityline-illustration.html">
+                        <div class="outer">
+                            <div class="inner portfolio-upa"></div>
+                            <div class="portfolio-text top no-gradient text-light">
+                                <div class="logo pull-left upa-logo"></div>
+                                <div class="logo-text pull-right">
+                                    <h5>L'Union des producteurs agricoles</h5>
+                                    <p>Illustration made for the UPA which was made into a TV commercial.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6" id="travelSmart">
+                    <a href="projects/travel-smart-canada-app.html">
+                        <div class="outer">
+                            <div class="inner portfolio-travel-smart"></div>
+                            <div class="portfolio-text bottom text-light">
+                                <div class="travel-smart-logo"></div>
+                                <div class="logo pull-left canada-logo"></div>
+                                <div class="pull-left">
+                                    <h5>Travel Smart</h5>
+                                    <p>Visual and user experience design for Global Affairs Canada’s Travel Smart app.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-sm-6" id="suite88">
+                    <a href="projects/suite88-cityline-illustration.html">
+                        <div class="outer">
+                            <div class="inner portfolio-suite88"></div>
+                            <div class="portfolio-text bottom no-gradient text-dark">
+                                <div class="logo pull-right suite88-logo"></div>
+                                <div class="logo-text pull-left">
+                                    <h5>Suite 88</h5>
+                                    <p>Illustration designed for Suite 88 limited edition chocolate packaging.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6" id="otis">
+                    <a href="projects/otis-elevator-animated-gif.html">
+                        <div class="outer">
+                            <div class="inner portfolio-otis"></div>
+                            <div class="portfolio-text bottom no-gradient text-dark">
+                                <div class="logo pull-left otis-logo"></div>
+                                <div class="logo-text pull-right">
+                                    <h5 class="text-blue-otis">Otis Elevator</h5>
+                                    <p>Animated GIF for an Otis Canada Holiday Greeting.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-sm-6" id="innolab">
+                    <a href="projects/global-affairs-canada-innovation-space.html">
+                        <div class="outer">
+                            <div class="inner portfolio-innovation-space"></div>
+                            <div class="portfolio-text top text-light">
+                                <div class="logo pull-left canada-logo"></div>
+                                <div class="logo-text pull-right">
+                                    <h5>Innovation Space</h5>
+                                    <p>Illustrations created for the new Innovation Room walls at Global Affairs Canada.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </section>
+
+        <!-- Brand identity section -->
+
+        <section class="portfolio">
+            <div class="row">
+                <div class="container">
+                    <h3>Brand identity projects</h3>
+                    <h4>A selection of brand identities designed by Loogart for various clients in different industries. </h4>
+                    <div class="scroll">
+                        <a href="#!"><img class="carousel-brands" src=""></a>
+                        <a href="#!"><img class="carousel-brands" src=""></a>
+                        <a href="#!"><img class="carousel-brands" src=""></a>
+                        <a href="#!"><img class="carousel-brands" src=""></a>
+                        <a href="#!"><img class="carousel-brands" src=""></a>
+                        <a href="#!"><img class="carousel-brands" src=""></a>
+                        <a href="#!"><img class="carousel-brands" src=""></a>
+                        <a href="#!"><img class="carousel-brands" src=""></a>
+                        <a href="#!"><img class="carousel-brands" src=""></a>
+                        <a href="#!"><img class="carousel-brands" src=""></a>
+                    </div>
+                    <h5><a href="#!">View all brand projects</a></h5>
+                </div>
+            </div>
+        </section>
+
+        <!-- Web design section -->
+
+        <section class="portfolio">
+            <div class="row">
+                <div class="container">
+                    <h3>Web design projects</h3>
+                    <h4>A selection of websites made with love by Loogart for several clients in different industries.</h4>
+                    <div class="scroll">
+                        <a href="#!"><img class="carousel-web-design" src=""></a>
+                        <a href="#!"><img class="carousel-web-design" src=""></a>
+                        <a href="#!"><img class="carousel-web-design" src=""></a>
+                        <a href="#!"><img class="carousel-web-design" src=""></a>
+                        <a href="#!"><img class="carousel-web-design" src=""></a>
+                        <a href="#!"><img class="carousel-web-design" src=""></a>
+                        <a href="#!"><img class="carousel-web-design" src=""></a>
+                        <a href="#!"><img class="carousel-web-design" src=""></a>
+                        <a href="#!"><img class="carousel-web-design" src=""></a>
+                        <a href="#!"><img class="carousel-web-design" src=""></a>
+                    </div>
+                    <h5><a href="#!">View all web projects</a></h5>
+                </div>
+            </div>
+        </section>
+
+</div>
+
+    <!-- footer -->
+    <?php include 'php/footer.php';?>
+
+</body>
+
+</html>
