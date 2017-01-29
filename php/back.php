b2b9f6d18cace1e54ae333cc1ed2012be90100da
Chris Soueidan <chris.soueidan@gmail.com>
2017-01-28 22:22:16 -0500

    added masonry, removed unnecessary js files, added new back php include

---
+++ php/back.php
@@ -0,0 +1,9 @@
+<?php
+echo '<div class="row">
+            <div class="col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
+                <p class="cta-back">
+                    <a href="./" role="button">Back</a>
+                </p>
+            </div>
+        </div>'
+;?>
