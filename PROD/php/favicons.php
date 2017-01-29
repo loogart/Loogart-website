5fab25891d52903aff86a053d6fefbdcc62dc267
Chris Soueidan <chris.soueidan@gmail.com>
2017-01-29 14:57:35 -0500

    fixed favicon issues

--- PROD/php/favicons.php
+++ PROD/php/favicons.php
@@ -1,17 +1,10 @@
 <?php
-echo '<!-- Add to homescreen for Chrome on Android -->
-    <meta name="mobile-web-app-capable" content="yes">
-    <link rel="icon" sizes="192x192" href="img/icons/android-desktop.png">
-
-    <!-- Add to homescreen for Safari on iOS -->
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black">
-    <meta name="apple-mobile-web-app-title" content="Loogart">
-    <link rel="apple-touch-icon-precomposed" href="img/icons/ios-desktop.png">
-
-    <!-- Tile icon for Win8 (144x144 + tile color) -->
-    <meta name="msapplication-TileImage" content="img/icons/ms-touch-icon-144x144-precomposed.png">
-    <meta name="msapplication-TileColor" content="#3372DF">
-
-    <link rel="shortcut icon" href="img/icons/favicon.png">'
+echo '<link rel="apple-touch-icon" sizes="180x180" href="img/icons/apple-touch-icon.png">
+<link rel="icon" type="image/png" href="img/icons/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="img/icons/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="img/icons/manifest.json">
+<link rel="mask-icon" href="img/icons/safari-pinned-tab.svg" color="#f75a56">
+<link rel="shortcut icon" href="img/icons/favicon.ico">
+<meta name="msapplication-config" content="img/icons/browserconfig.xml">
+<meta name="theme-color" content="#f75a56">'
 ;?>
