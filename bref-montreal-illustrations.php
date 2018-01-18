b5e9b74dff0cae678f39441086c0f14b9351b29c
Chris Soueidan <chris@loogart.com>
2018-01-18 12:05:54 -0500

    added content for bref portfolio page

--- bref-montreal-illustrations.php
+++ bref-montreal-illustrations.php
@@ -10,63 +10,105 @@
     <meta property="og:title" content="Bref x Loogart Montréal Illustrations">
     <meta property="og:description" content="Encore Montreal City Illustrations by Loogart">
     <meta property="og:url" content="http://loogart.com/">
-    <meta property="og:image" content="http://loogart.com/img/portfolio/bref/.jpg">
+    <meta property="og:image" content="http://loogart.com/img/portfolio/bref/illustration_encore-montreal-city_by-loogart.png">
     <title>Bref x Loogart Encore Montréal Illustrations | Loogart</title>
     <?php include 'php/header.php';?>
     <?php include 'php/facebook-pixel.php';?>
 </head>
 
 <body>
-   
+
     <?php include './php/analyticstracking.php';?>
     <?php include './php/navbar.php';?>
 
     <main>
-        <div class="project-jumbotron" style="height:24vh;background-color:#ccc"></div>
+        <img src="./img/portfolio/bref/photo-bref-loogart-frames-wall.jpg" class="img-fit" alt="Encore Montreal Wall by Loogart at Bref MTL">
         <div class="container-fluid">
 
-        <?php include './php/back.php';?>
+            <?php include './php/back.php';?>
 
-        <!-- Content section -->
-        <section class="portfolio-content">
+            <!-- Content section -->
+            <section class="portfolio-content">
 
-            <!-- First section: Starbucks Montreal -->
-            <div class="row">
-                <div class="col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
-
-                    <img class="img-responsive" src="./img/logo/logo-bref-mtl.png" style="max-width:120px;margin:30px 0">
-                    <h1>Encore Montréal Illustrations by Loogart</h1>
-                    <p>Bref is a Montreal-based boutique, gallery and event venue who’s mission is to offer art and design lovers a unique shopping experience. Every month, they bring together, under a unified theme, exclusive articles created by local and international emerging artists and brands.</p>
-
-                    <h3>Bref x Loogart</h3>
-                    <p>Loogart was approached to be a part of the awesome “Bref, vivez Montréal!” theme, which complemented the 375th anniversary of Montreal. The partnership between Loogart and Bref was such an inspiring one. We created 8 new illustrations which would represent specific neighbourhoods in Montreal: Montreal Island, Little Italy, Mile-End, Le Plateau, Saint-Henri, Griffintown, Le Village and Hochelaga-Maisonneuve.</p>
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <img class="img-responsive img-spacing" src="./img/portfolio/bref/" data-action="zoom">
+                <!-- First section: Starbucks Montreal -->
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <img class="img-responsive" src="./img/logo/logo-bref-mtl.png" style="max-width:120px;margin:30px 0">
+                        <h1>Encore Montréal Illustrations by Loogart</h1>
+                        <p>Bref is a Montreal-based boutique, gallery and event venue who’s mission is to offer art and design lovers a unique shopping experience. Every month, they bring together, under a unified theme, exclusive articles created by local and international emerging artists and brands.</p>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <img class="img-responsive img-spacing" src="./img/portfolio/bref/photo-bref-loogart-frames-decal-par-jeanluc.jpg" alt="Photo of Encore MTL artwork by Loogart by Jean luc">
+                            </div>
                         </div>
-                        <div class="col-sm-6">
-                            <img class="img-responsive img-spacing" src="./img/portfolio/bref/" data-action="zoom">
+                        <h3>Bref x Loogart</h3>
+                        <p>Loogart was approached to be a part of the awesome “Bref, vivez Montréal!” theme, which complemented the 375th anniversary of Montreal. The partnership between Loogart and Bref was such an inspiring one. We created 8 new illustrations which would represent specific neighbourhoods in Montreal: Montreal Island, Little Italy, Mile-End, Le Plateau, Saint-Henri, Griffintown, Le Village and Hochelaga-Maisonneuve.</p>
+                        <div class="row">
+                            <div class="col-sm-12 hover-zoom">
+                                <img class="img-responsive img-spacing" src="./img/portfolio/bref/illustration_encore-montreal-city_by-loogart.png" data-action="zoom" alt="Illustration of Encore Montreal by Loogart">
+                                <img class="img-responsive img-spacing" src="./img/portfolio/bref/illustration_encore-montreal-petite-italie_by-loogart.png" data-action="zoom" alt="Illustration of Encore Montreal - Petite Italy by Loogart">
+                                <img class="img-responsive img-spacing" src="./img/portfolio/bref/illustration_encore-montreal_mile-end_by-loogart.png" data-action="zoom" alt="Illustration of Encore Montreal - Mile End by Loogart">
+                                <img class="img-responsive img-spacing" src="./img/portfolio/bref/illustration_encore-montreal_le-plateau_by-loogart.png" data-action="zoom" alt="Illustration of Encore Montreal - Le Plateau by Loogart">
+                                <img class="img-responsive img-spacing" src="./img/portfolio/bref/illustration_encore-montreal_saint-henri_by-loogart.png" data-action="zoom" alt="Illustration of Encore Montreal - Saint-Henri by Loogart">
+                                <img class="img-responsive img-spacing" src="./img/portfolio/bref/illustration_encore-montreal_griffintown_by-loogart.png" data-action="zoom" alt="Illustration of Encore Montreal - Griffintown by Loogart">
+                                <img class="img-responsive img-spacing" src="./img/portfolio/bref/illustration_encore-montreal_le-village_by-loogart.png" data-action="zoom" alt="Illustration of Encore Montreal - Le Village by Loogart">
+                                <img class="img-responsive img-spacing" src="./img/portfolio/bref/illustration_encore-montreal_hochelaga-maisonneuve_by-loogart.png" data-action="zoom" alt="Illustration of Encore Montreal - Hochelaga Maisonneuve by Loogart">
+                            </div>
                         </div>
+                        <h3>Illustration style</h3>
+                        <p>The illustrations were designed to fit a square which constrains the design area, thus requiring artistic simplification of the elements and landmarks within each one. Style consistency was implemented by building a limited colour palette and by style i.e. thick bold lines married with circular anchors - reminiscent of the original Montreal CityLine collection.</p>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <img class="img-responsive img-spacing" src="./img/portfolio/bref/photo-tshirt-loogart-bref.jpg" data-action="zoom" alt="Photo of Encore MTL limited edition t-shirt">
+                            </div>
+                        </div>
+
+                        <h3>Photos of Bref MTL Products</h3>
+                        <p>Limited edition products were made through the Bref x Loogart collaboration which you buy directly at the gallery or via their <a href="https://www.brefmtl.com/boutique" target="_blank">online shop</a>. These products include prints, t-shirts, stamps, drinkware and postcards.</p>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <img class="img-responsive img-spacing" src="./img/portfolio/bref/photo-bref-loogart-glass-mugs.jpg" data-action="zoom" alt="Photo of Encore MTL limited edition glass mugs ">
+                            </div>
+                            <div class="col-sm-6">
+                                <img class="img-responsive img-spacing" src="./img/portfolio/bref/photo-verre-loogart-bref.jpg" data-action="zoom" alt="Photo of one Encore MTL limited edition glass mug">
+                            </div>
+                            <div class="col-sm-6">
+                                <img class="img-responsive img-spacing" src="./img/portfolio/bref/photo-verre-loogart-bref-2.jpg" data-action="zoom" alt="Photo of one Encore MTL limited edition glass mug detail">
+                            </div>
+                            <div class="col-sm-6">
+                                <img class="img-responsive img-spacing" src="./img/portfolio/bref/photo-carte-postale-loogart-bref.jpg" data-action="zoom" alt="Photo of one Encore MTL Saint-Henri limited edition post card">
+                            </div>
+                            <div class="col-sm-6">
+                                <img class="img-responsive img-spacing" src="./img/portfolio/bref/photo-carte-postale-loogart-bref-2.jpg" data-action="zoom" alt="Photo of the back of one Encore MTL limited edition post card">
+                            </div>
+                            <div class="col-sm-6">
+                                <img class="img-responsive img-spacing" src="./img/portfolio/bref/photo-timbre-loogart-bref.jpg" data-action="zoom" alt="Photo of the Encore MTL limited edition stamp set">
+                            </div>
+                            <div class="col-sm-6">
+                                <img class="img-responsive img-spacing" src="./img/portfolio/bref/photo-timbre-loogart-bref-2.jpg" data-action="zoom" alt="Detail photo of the Encore MTL limited edition stamp set">
+                            </div>
+                            <div class="col-sm-offset-2 col-sm-8">
+                                <img class="img-responsive img-spacing" src="./img/portfolio/bref/photo-bref-loogart-products.JPG" data-action="zoom" alt="Photo of Encore MTL limited edition glass, stamps and post card">
+                            </div>
+                        </div>
+
+
                     </div>
-
-                    <p>The illustrations were designed to fit a square which constrains the design area, thus requiring artistic simplification of the elements and landmarks within each one. Style consistency was implemented by building a limited colour palette and by style i.e. thick bold lines married with circular anchors - reminiscent of the original Montreal CityLine collection.</p>
-
-                    <p>Limited edition products were made through the Bref x Loogart collaboration which you buy directly at the gallery or via their only shop. These products include prints, t-shirts, stamps, drinkware and postcards.</p>
-
-
                 </div>
-            </div>
 
-        </section>
+            </section>
 
-        <?php include './php/back.php';?>
+            <?php include './php/back.php';?>
 
-    </div>
+        </div>
     </main>
-    
+
     <?php include './php/footer.php';?>
-    <script>fbq('track', 'ViewBref');</script>
-    
+    <script>
+        fbq('track', 'ViewBref');
+
+    </script>
+
 </body>
 
 </html>
