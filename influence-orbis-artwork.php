aeb2a487e3f69f9db992a55b3418a6a634d8c7d4
Chris Soueidan <chris@loogart.com>
2018-01-18 15:43:26 -0500

    influence social media section
    embed social media posts

--- influence-orbis-artwork.php
+++ influence-orbis-artwork.php
@@ -68,7 +68,29 @@
                             </div>
                         </div>
                         <h3>Event photography</h3>
-                        <p>The artwork impressed the participants and the guest speakers who were given a canvas with their Loogmoji. The Loogmojis were shared on social media by the influencers on their Twitter feed, Instagram accounts and Facebook pages.</p>
+                        <p>The artwork impressed the participants and the guest speakers who were given a canvas with their Loogmoji. The Loogmojis were shared on social media by the influencers on their <a href="https://twitter.com/CaseyNeistat/status/933016922471436288" target="_blank">Twitter feed</a>, <a href="https://www.instagram.com/p/BbxbQnWltRI/?saved-by=loogart" target="_blank">Instagram accounts</a> and <a href="https://www.facebook.com/wealthylifers/posts/10101123499999201" target="_blank">Facebook pages</a>.</p>
+                        <div class="col-sm-offset-2 col-sm-8">
+                            <blockquote class="twitter-tweet" data-lang="en">
+                                <p lang="en" dir="ltr">which is the real Casey? <a href="https://t.co/1x3W5oBSio">pic.twitter.com/1x3W5oBSio</a></p>&mdash; Casey Neistat (@CaseyNeistat) <a href="https://twitter.com/CaseyNeistat/status/933016922471436288?ref_src=twsrc%5Etfw">November 21, 2017</a></blockquote>
+                            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                        </div>
+                        <div class="col-sm-6">
+                            <blockquote class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/BbxbQnWltRI/" data-instgrm-version="8" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);">
+                                <div style="padding:8px;">
+                                    <div style=" background:#F8F8F8; line-height:0; margin-top:40px; padding:62.4537037037037% 0; text-align:center; width:100%;">
+                                        <div style=" background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACwAAAAsCAMAAAApWqozAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAMUExURczMzPf399fX1+bm5mzY9AMAAADiSURBVDjLvZXbEsMgCES5/P8/t9FuRVCRmU73JWlzosgSIIZURCjo/ad+EQJJB4Hv8BFt+IDpQoCx1wjOSBFhh2XssxEIYn3ulI/6MNReE07UIWJEv8UEOWDS88LY97kqyTliJKKtuYBbruAyVh5wOHiXmpi5we58Ek028czwyuQdLKPG1Bkb4NnM+VeAnfHqn1k4+GPT6uGQcvu2h2OVuIf/gWUFyy8OWEpdyZSa3aVCqpVoVvzZZ2VTnn2wU8qzVjDDetO90GSy9mVLqtgYSy231MxrY6I2gGqjrTY0L8fxCxfCBbhWrsYYAAAAAElFTkSuQmCC); display:block; height:44px; margin:0 auto -44px; position:relative; top:-22px; width:44px;"></div>
+                                    </div>
+                                    <p style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; line-height:17px; margin-bottom:0; margin-top:8px; overflow:hidden; padding:8px 0 7px; text-align:center; text-overflow:ellipsis; white-space:nowrap;"><a href="https://www.instagram.com/p/BbxbQnWltRI/" style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:normal; line-height:17px; text-decoration:none;" target="_blank">A post shared by Charlamagne Thagod (@cthagod)</a> on <time style=" font-family:Arial,sans-serif; font-size:14px; line-height:17px;" datetime="2017-11-21T21:24:52+00:00">Nov 21, 2017 at 1:24pm PST</time></p>
+                                </div>
+                            </blockquote>
+                            <script async defer src="//platform.instagram.com/en_US/embeds.js"></script>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="fb-post" data-href="https://www.facebook.com/wealthylifers/posts/10101123499999201" data-width="500" data-show-text="false">
+                                <blockquote cite="https://www.facebook.com/wealthylifers/posts/10101123499999201" class="fb-xfbml-parse-ignore">
+                                    <p>#InfluenceToronto2017 you sure know how to make a gal feel special! Enercare Centre Thanks Chris Soueidan and Thierry Lindor!! #Loogmoji</p>Posted by <a href="https://www.facebook.com/wealthylifers">Angel Rich</a> on&nbsp;<a href="https://www.facebook.com/wealthylifers/posts/10101123499999201">Tuesday, November 21, 2017</a></blockquote>
+                            </div>
+                        </div>
                         <div class="col-sm-12">
                             <img class="img-responsive img-spacing" src="./img/portfolio/influence-toronto/photo-too-funky-toronto-by-loogart-2.jpg" data-action="zoom" alt="Toronto City Too Funky by Loogart framed prints">
                         </div>
--- php/analyticstracking.php
+++ php/analyticstracking.php
@@ -1,10 +1,29 @@
+<div id="fb-root"></div>
 <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11&appId=1502111493180438';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
 
-  ga('create', 'UA-28021804-1', 'auto');
-  ga('send', 'pageview');
+<script>
+    (function(i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function() {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-28021804-1', 'auto');
+    ga('send', 'pageview');
 
 </script>
