b25327be41ff5fee593e534f17ab1f947724107e
Chris Soueidan <chris@loogart.com>
2018-01-17 22:17:26 -0500

    yp work

--- yellow-pages-icons-and-illustrations.php
+++ yellow-pages-icons-and-illustrations.php
@@ -19,53 +19,61 @@
 <body>
     <?php include './php/analyticstracking.php';?>
     <?php include './php/navbar.php';?>
-    
+
     <main>
-        <div class="project-jumbotron" style="height:36vh;background-color:#FFCD00;background-image:url('../img/portfolio/yellow-pages/yellow-pages-citysacpe-by-loogart-montreal-1.jpg');background-position:center"></div>
+        <!--        <div class="project-jumbotron" style="height:36vh;background-color:#FFCD00;background-image:url('../img/portfolio/yellow-pages/yellow-pages-citysacpe-by-loogart-montreal-1.jpg');background-position:center"></div>-->
+        <img src="./img/portfolio/yellow-pages/jumbotron-yellow-pages-loogart.jpg" class="img-responsive" style="width:100%">
         <div class="container-fluid">
 
-        <?php include './php/back.php';?>
+            <?php include './php/back.php';?>
 
-        <!-- Content section -->
-        <section class="portfolio-content">
+            <!-- Content section -->
+            <section class="portfolio-content">
 
-            <!-- First section: Starbucks Montreal -->
-            <div class="row">
-                <div class="col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+                <!-- First section: Starbucks Montreal -->
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
 
-                    <img class="img-responsive" src="./img/logo/logo-yellow-pages-2.png" style="max-width:120px;margin:30px 0">
-                    <h1>Yellow Pages iconography and illustrations</h1>
-                    <p>Loogart works with companies to enhance their brand through illustration, graphic design and user interface design. Yellow Pages worked with award-winning visual identity design firm Turner Duckworth to create a new visual language which would grab the attention of a younger, small-business entrepreneurial crowd. Yellow Pages also reached out to Loogart while they were in the midst of this rebranding effort prioritizing the more handcrafted look.</p>
-                    
-                    <h3>Handmade and Cut paper</h3>
-                    <p>We gladly took on the challenge to create visuals that blended perfectly within the new brand system defined by Turner Duckworth. This included the creation of suite of icons, web illustrations and print cityscape designs. While the final logo has yet to be revealed by Yellow Pages, you can notice the updated look and feel through the visuals we created - Handmade and Cut Paper.</p>
-                    
-                    <h3>App onboarding user interface</h3>
-                    <p>Loogart was also involved for quick prototyping of the onboarding process in the Yellow Pages iOS app. This was mainly an exercise to evaluate how Yellow Pages could implement their new visual language and assets into their existing products. </p>
-                    <div class="row">
-                        <div class="col-sm-12">
-                            <img class="img-responsive img-spacing" src="./img/portfolio/yellow-pages/" data-action="zoom">
+                        <img class="img-responsive" src="./img/logo/logo-yellow-pages-2.png" style="max-width:120px;margin:30px 0">
+                        <h1>Yellow Pages iconography and illustrations</h1>
+                        <p>Loogart works with companies to enhance their brand through illustration, graphic design and user interface design. Yellow Pages worked with award-winning visual identity design firm Turner Duckworth to create a new visual language which would grab the attention of a younger, small-business entrepreneurial crowd. Yellow Pages also reached out to Loogart while they were in the midst of this rebranding effort prioritizing the more handcrafted look.</p>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <img class="img-responsive img-spacing" src="./img/portfolio/yellow-pages/yellow-pages_icons-set1_by-loogart.jpg" data-action="zoom">
+                            </div>
+                            <div class="col-sm-12">
+                                <img class="img-responsive img-spacing" src="./img/portfolio/yellow-pages/yellow-pages_icons-set2_by-loogart.jpg" data-action="zoom">
+                            </div>
                         </div>
+
+                        <h3>Handmade and Cut paper</h3>
+                        <p>We gladly took on the challenge to create visuals that blended perfectly within the new brand system defined by Turner Duckworth. This included the creation of suite of icons, web illustrations and print cityscape designs. While the final logo has yet to be revealed by Yellow Pages, you can notice the updated look and feel through the visuals we created - Handmade and Cut Paper.</p>
+
+                        <h3>App onboarding user interface</h3>
+                        <p>Loogart was also involved for quick prototyping of the onboarding process in the Yellow Pages iOS app. This was mainly an exercise to evaluate how Yellow Pages could implement their new visual language and assets into their existing products. </p>
+
+
+                        <h3>App onboarding user interface</h3>
+                        <p>Successful branding evokes proper emotion and we believe that creating a full-range of visuals to supports the brand effectively and inspires action and feelings of trust with their existing client base and prospective clients. Moreover, Loogart created 3 cityscape illustrations for the Yellow Pages HQ offices walls in Montreal, Toronto and Vancouver.</p>
+
+                        <h3>Yellow Pages B2B</h3>
+                        <p>You can view some of the illustrations created by Loogart for Yellow Pages on their <a href="https://business.yellowpages.ca/why-yellow-pages" target="_blank">B2B website</a>.</p>
                     </div>
-                    
-                    <h3>App onboarding user interface</h3>
-                    <p>Successful branding evokes proper emotion and we believe that creating a full-range of visuals to supports the brand effectively and inspires action and feelings of trust with their existing client base and prospective clients. Moreover, Loogart created 3 cityscape illustrations for the Yellow Pages HQ offices walls in Montreal, Toronto and Vancouver.</p>
-                    
-                    <h3>Yellow Pages B2B</h3>
-                    <p>You can view some of the illustrations created by Loogart for Yellow Pages on their <a href="https://business.yellowpages.ca/why-yellow-pages" target="_blank">B2B website</a>.</p>
                 </div>
-            </div>
 
-        </section>
+            </section>
 
-        <?php include './php/back.php';?>
+            <?php include './php/back.php';?>
 
-    </div>
+        </div>
     </main>
-    
+
     <?php include './php/footer.php';?>
-    <script>fbq('track', 'ViewYellowPages');</script>
-    
+    <script>
+        fbq('track', 'ViewYellowPages');
+
+    </script>
+
 </body>
 
 </html>
