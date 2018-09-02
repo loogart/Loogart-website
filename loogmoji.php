916c47a67e847144c8594b077cf1ecc83deebf54
Chris Soueidan <chris@loogart.com>
2018-09-02 12:57:03 -0400

    updated loogmoji link

--- loogmoji.php
+++ loogmoji.php
@@ -295,12 +295,7 @@
                         <h3>Want to see more Loogmojis?</h3>
                         <p>Follow Loogart on <a href="https://www.instagram.com/loogart/" target="_blank">Instagram</a>, you'll see the latest <a href="https://www.instagram.com/explore/tags/loogmoji/" target="_blank">#Loogmojis</a>! If you want to get your own Loogmoji, you can <a href="http://10x.loogart.com" target="_blank">purchase it here</a>! Once you get your own Loogmoji, you'll be part of the family and we will add you to this page.</p>
                         <div class="row">
-                            <div class="col-sm-4">
-                                <div class="tictail-embed-frame" data-id="T9rL" data-type="product" data-nofollow="false" data-optin="false" data-reviews="false" data-showlogo="false" style="margin:auto;"><a href="http://loogart.tictail.com/product/original-loogmoji?utm_source=tictail.com&utm_medium=embed&utm_campaign=v2" rel="nofollow"><span>Browse Tictail</span></a><a href="http://loogart.tictail.com/product/original-loogmoji?utm_source=tictail.com&utm_medium=embed&utm_campaign=v2" rel="nofollow"> - Loogart Shop</a>
-                                    <script async="" src="https://frontend.ttcdn.co/embed/production/tt-embed.js"></script>
-                                </div>
-                            </div>
-                            <div class="col-sm-4">
+                            <div class="col-sm-6">
                                 <blockquote class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/BcibzeXh_zc/" data-instgrm-version="8" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);">
                                     <div style="padding:8px;">
                                         <div style=" background:#F8F8F8; line-height:0; margin-top:40px; padding:50.0% 0; text-align:center; width:100%;">
@@ -311,7 +306,7 @@
                                 </blockquote>
                                 <script async defer src="//platform.instagram.com/en_US/embeds.js"></script>
                             </div>
-                            <div class="col-sm-4">
+                            <div class="col-sm-6">
                                 <blockquote class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/BG6yokbhlrS/" data-instgrm-version="8" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);">
                                     <div style="padding:8px;">
                                         <div style=" background:#F8F8F8; line-height:0; margin-top:40px; padding:50.0% 0; text-align:center; width:100%;">
@@ -321,6 +316,11 @@
                                     </div>
                                 </blockquote>
                                 <script async defer src="//platform.instagram.com/en_US/embeds.js"></script>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="tictail-embed-frame" data-id="T9rL" data-type="product" data-nofollow="false" data-optin="false" data-reviews="false" data-showlogo="false" style="margin:auto;"><a href="http://loogart.tictail.com/product/original-loogmoji?utm_source=tictail.com&utm_medium=embed&utm_campaign=v2" rel="nofollow"><span>Browse Tictail</span></a><a href="http://loogart.tictail.com/product/original-loogmoji?utm_source=tictail.com&utm_medium=embed&utm_campaign=v2" rel="nofollow"> - Loogart Shop</a>
+                                    <script async="" src="https://frontend.ttcdn.co/embed/production/tt-embed.js"></script>
+                                </div>
                             </div>
                         </div>
                     </div>
