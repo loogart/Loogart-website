041c182b5c3d7c2dd38a91b5f7d32669d63e84a3
Chris Soueidan <chris.soueidan@gmail.com>
2019-01-03 17:31:25 -0500

    Update shop-custom-loogmoji.php

--- shop-custom-loogmoji.php
+++ shop-custom-loogmoji.php
@@ -14,6 +14,17 @@
     <title>Buy Loogmoji | Loogart</title>
     <?php include 'php/header.php';?>
     <?php include 'php/facebook-pixel.php';?>
+    <!-- Hotjar Tracking Code for https://loogart.com/shop-custom-loogmoji -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1145479,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
 </head>
 
 <body>
