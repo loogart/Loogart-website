44d94aeebb19d25016cc5da19366786beaeebb1d
Chris Soueidan <chris@loogart.com>
2020-10-15 14:41:23 -0400

    added tv show page

---
+++ wacky-word-show.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0">
+    <meta name="description" content="Loogart is a visual design studio that specializes in illustration, brand identity, small-scale animation and web design. Our work is featured in Montreal, Toronto, Ottawa and Gatineau.">
+    <meta name="author" content="Chris Soueidan of Loogart">
+    <meta property="og:title" content="Loogart">
+    <meta property="og:description" content="Montreal-based Visual Design Studio">
+    <meta property="og:url" content="http://loogart.com/">
+    <meta property="og:image" content="https://loogart.com/img/logo/chris-of-loogart.jpg">
+    <title>Loogart on the Wacky Word Show | Loogart</title>
+    <?php include 'php/header.php';?>
+    <?php include 'php/facebook-pixel.php';?>
+</head>
+
+<body>
+
+    <?php include 'php/analyticstracking.php';?>
+    <?php include './php/navbar.php';?>
+
+    <main>
+<!--        <section class="jumbotron-about">-->
+            
+<!--        </section>-->
+        <!-- 16:9 aspect ratio -->
+        <div class="container" style="margin:29px auto 69px;">
+            <div class="row">
+                <div class="col-md-offset-2 col-md-8">
+                   <h1>Watch Loogart on the Wacky Word Show.</h1>
+                   <p style="margin-bottom:20px;">october 7, 2019 on TVOkids</p>
+                    
+                   <a href="http://bit.ly/loogart-wacky-word-show" target="_blank">
+                    <img class="img-responsive" src="./img/video-2.jpg" alt="" style="border-radius:24px;box-shadow:
+  0 2.8px 2.2px rgba(0, 0, 0, 0.02),
+  0 6.7px 5.3px rgba(0, 0, 0, 0.028),
+  0 12.5px 10px rgba(0, 0, 0, 0.035),
+  0 22.3px 17.9px rgba(0, 0, 0, 0.042),
+  0 41.8px 33.4px rgba(0, 0, 0, 0.05),
+  0 100px 80px rgba(0, 0, 0, 0.07)
+;
+">
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    </main>
+
+    <?php include './php/footer.php';?>
+    <script>
+        fbq('track', 'ViewAbout');
+
+    </script>
+
+</body>
+
+</html>
